0f8ef61706b1da95b710950f848fcd39b2f687a5
Jason MacKeigan <w0270109@nscc.ca>
2022-03-11 10:55:24 -0400

    Added routes to config, now we can hit projects service from port 8079.

--- CapstoneAPIGateway/src/main/java/org/gangreen/CapstoneGatewayApplication.java
+++ CapstoneAPIGateway/src/main/java/org/gangreen/CapstoneGatewayApplication.java
@@ -2,10 +2,10 @@ package org.gangreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableDiscoveryClient
 public class CapstoneGatewayApplication {
 
     public static void main(String[] args) {
